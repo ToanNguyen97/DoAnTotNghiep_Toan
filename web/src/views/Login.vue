06999665d40e539bee2714b5f6a3d35c11e5e34f
Văn Toàn Nguyễn <toan210597ntu@gmail.com>
2019-04-10 14:39:23 +0700

    design xong form login

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,47 +1,29 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <v-btn
-                    slot="activator"
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                  >
-                    <v-icon large>code</v-icon>
-                  </v-btn>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank">
-                    <v-icon large>mdi-codepen</v-icon>
-                  </v-btn>
-                  <span>Codepen</span>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field prepend-icon="person" name="Username" v-model="username" label="username" type="text"></v-text-field>
-                  <v-text-field id="password" prepend-icon="lock" v-model="password" name="password" label="Password" type="password"></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="Login">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+  <v-app id="inspire" class="loginForm">
+    <div class="loginbox">
+      <h1 style="text-align:center">Phòng Trọ Cô Mai</h1>
+      <hr>
+      <v-flex style="display:flex" justify-center>
+        <div class="pt-4" style="width:400px;">
+          <v-text-field append-icon="people" v-model="username"  type="text" hide-details background-color="white" dark label="Tên Đăng Nhập" outline ></v-text-field>
+        </div>
+      </v-flex>
+      <v-flex style="display:flex" justify-center>
+        <div class="pt-4" style="width:400px;">
+          <v-text-field  append-icon="lock" v-model="password" @keyup.enter="Login" type="passWord" hide-details background-color="white" dark label="Mật Khẩu" outline ></v-text-field>
+        </div>
+      </v-flex>
+      <v-flex style="display:flex" justify-center>
+        <div class="pt-4" style="width:300px;text-align: center;">
+          <v-btn color="cyan" @click="Login" dark>Đăng Nhập</v-btn>
+        </div>
+      </v-flex>
+      <v-flex style="display:flex" justify-end>
+        <div class="pt-4" style="width:300px;text-align: center;">
+          <a href="" class="white--text">Quên mật khẩu?</a>        
+        </div>
+      </v-flex>
+    </div>
   </v-app>
 </template>
 
@@ -88,3 +70,20 @@
     }
   }
 </script>
+<style scoped>
+  #inspire {
+    background: url(../assets/login.jpg) no-repeat;
+    background-size: cover;
+  }
+  .loginbox {
+    position: absolute;
+    width: 589px;
+    height: 350px;
+    top:50%;
+    left: 50%;
+    background: rgba(0,0,0,.8);
+    color: white;
+    transform: translate(-50%,-50%);
+    border-radius: 12px;
+  }
+</style>
