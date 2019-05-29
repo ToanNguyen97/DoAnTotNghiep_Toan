262f7d5880e1d5d6f4571923b99a975ed4290785
Văn Toàn Nguyễn <toan210597ntu@gmail.com>
2019-05-29 11:08:49 +0700

    animation

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -2,7 +2,9 @@
   <div class="admin" style="display: flex; flex: 1 1 auto; flex-direction: column" >
     <Toolbar @input="input"/>
     <v-content class="ml-4" style="padding: 70px 0px 0px 70px; display: flex; flex: 1 1 auto;" :class="!mini ? 'padding-left-300' : ''">
-      <router-view></router-view>  
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>  
+      </transition>
     </v-content>
     <v-content class="ml-4" style="padding: 5px 0px 0px 70px; display: flex; flex: 0 0 auto;" :class="!mini ? 'padding-left-300' : ''">
        <Footer />
